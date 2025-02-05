cc2c789c7b1748a9a5b2bd2fbc539bb54a14fec1
Tom Coates <tom@soothsys.com>
2025-02-05 02:48:35 +0000

    Move ESP32 code to subdirectory, change data format to float, support IAQ/static IAQ

--- glove/bme688.cpp
+++ glove/bme688.cpp
@@ -18,25 +18,30 @@
 #include "i2c_address.h"
 #include "err.h"
 
-#define	SAMPLE_RATE					BSEC_SAMPLE_RATE_LP
+#define	SAMPLE_RATE					BSEC_SAMPLE_RATE_CONT
 #define TEMP_OFFSET					TEMP_OFFSET_LP
 
-#define BLE_SERVICE_UUID			0x181A
-#define DESCRIPTOR_UUID				0x2902
-#define TEMP_CHARACTERISTIC_UUID	0x2A6E
-#define HUM_CHARACTERISTIC_UUID		0x2A6F
-#define PRES_CHARACTERISTIC_UUID	0x2A6D
-#define TEMP_SCALE    				100.0f
-#define HUM_SCALE   				100.0f
-#define PRES_SCALE  				100.0f
+#define BLE_NUM_HANDLES       16 //Need more than the default 15 handles due to large number of characteristics
+#define BLE_INST_ID           0
+#define BLE_SERVICE_UUID			BLEUUID((uint16_t)0x181A)
+#define DESCRIPTOR_UUID				BLEUUID((uint16_t)0x2902)
+#define TEMP_CHARACTERISTIC_UUID	BLEUUID((uint16_t)0x2A6E)
+#define HUM_CHARACTERISTIC_UUID		BLEUUID((uint16_t)0x2A6F)
+#define PRES_CHARACTERISTIC_UUID	BLEUUID((uint16_t)0x2A6D)
+#define IAQ_CHARACTERISTIC_UUID   "b52338a6-b7fa-47d9-8db4-dbb86ac6b05c" //Custom UUID - IAQ does not exist in BLE spec
+#define SIAQ_CHARACTERISTIC_UUID  "0d1ab684-14a4-479b-9dcd-86b6fc2e99fa"
 
-BLECharacteristic m_tempCharacteristic(BLEUUID((uint16_t)TEMP_CHARACTERISTIC_UUID), BLECharacteristic::PROPERTY_NOTIFY);
-BLECharacteristic m_humCharacteristic(BLEUUID((uint16_t)HUM_CHARACTERISTIC_UUID), BLECharacteristic::PROPERTY_NOTIFY);
-BLECharacteristic m_presCharacteristic(BLEUUID((uint16_t)PRES_CHARACTERISTIC_UUID), BLECharacteristic::PROPERTY_NOTIFY);
+BLECharacteristic m_tempCharacteristic(TEMP_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_NOTIFY);
+BLECharacteristic m_humCharacteristic(HUM_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_NOTIFY);
+BLECharacteristic m_presCharacteristic(PRES_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_NOTIFY);
+BLECharacteristic m_iaqCharacteristic(IAQ_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_NOTIFY);
+BLECharacteristic m_siaqCharacteristic(SIAQ_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_NOTIFY);
 
-BLEDescriptor m_tempDescriptor(BLEUUID((uint16_t)DESCRIPTOR_UUID));
-BLEDescriptor m_humDescriptor(BLEUUID((uint16_t)DESCRIPTOR_UUID));
-BLEDescriptor m_presDescriptor(BLEUUID((uint16_t)DESCRIPTOR_UUID));
+BLEDescriptor m_tempDescriptor(DESCRIPTOR_UUID);
+BLEDescriptor m_humDescriptor(DESCRIPTOR_UUID);
+BLEDescriptor m_presDescriptor(DESCRIPTOR_UUID);
+BLEDescriptor m_iaqDescriptor(DESCRIPTOR_UUID);
+BLEDescriptor m_siaqDescriptor(DESCRIPTOR_UUID);
 
 Bsec2 m_envSensor;
 
@@ -67,31 +72,35 @@ void newDataCallback(const bme68xData data, const bsecOutputs outputs, Bsec2 bse
 	for (i = 0; i < outputs.nOutputs; i++) {
 		const bsecData output = outputs.output[i];
 		BLECharacteristic *pCharacteristic = NULL;
-		float scaleFactor = 1.0f;
 
 		switch (output.sensor_id) {
 			case BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_TEMPERATURE:
 				pCharacteristic = &m_tempCharacteristic;
-				scaleFactor = TEMP_SCALE;
 				break;
 			
 			case BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_HUMIDITY:
 				pCharacteristic = &m_humCharacteristic;
-				scaleFactor = HUM_SCALE;
 				break;
 				
 			case BSEC_OUTPUT_RAW_PRESSURE:
 				pCharacteristic = &m_presCharacteristic;
-				scaleFactor = PRES_SCALE;
 				break;
+
+      case BSEC_OUTPUT_IAQ:
+        pCharacteristic = &m_iaqCharacteristic;
+        break;
+
+      case BSEC_OUTPUT_STATIC_IAQ:
+        pCharacteristic = &m_siaqCharacteristic;
+        break;
 				
 			default:
 				break;
 		}
 		
 		if (pCharacteristic != NULL) {
-			uint16_t uval = (uint16_t)(output.signal * scaleFactor);
-			pCharacteristic->setValue(uval);
+      float val = output.signal;
+			pCharacteristic->setValue(val);
 			pCharacteristic->notify();
 		}
 	}
@@ -101,9 +110,9 @@ bool bme688_init(i2c_address_t addr) {
 	bsecSensor sensorList[] = {
 		BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_TEMPERATURE,
 		BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_HUMIDITY,
-		BSEC_OUTPUT_RAW_PRESSURE/*,							//No compensated pressure option exists
+		BSEC_OUTPUT_RAW_PRESSURE,							//No compensated pressure option exists
 		BSEC_OUTPUT_IAQ,									//Raw IAQ measurement
-		BSEC_OUTPUT_STATIC_IAQ,								//"Delta" IAQ measurement relative to running average, intended to show changes in air quality against "normal" value for location
+		BSEC_OUTPUT_STATIC_IAQ/*,								//"Delta" IAQ measurement relative to running average, intended to show changes in air quality against "normal" value for location
 		BSEC_OUTPUT_CO2_EQUIVALENT,
 		BSEC_OUTPUT_BREATH_VOC_EQUIVALENT,
 		BSEC_OUTPUT_STABILIZATION_STATUS,					//0 = stabilisation in progress, 1 = stabilisation finished
@@ -128,7 +137,7 @@ bool bme688_init(i2c_address_t addr) {
 }
 
 bool bme688_addServices(BLEServer *pServer) {
-	BLEService *pService = pServer->createService(BLEUUID((uint16_t)BLE_SERVICE_UUID));
+	BLEService *pService = pServer->createService(BLE_SERVICE_UUID, BLE_NUM_HANDLES, BLE_INST_ID);
 	if (pService == NULL) {
 		ERROR("Cannot add BLE service");
 		return false;
@@ -137,9 +146,13 @@ bool bme688_addServices(BLEServer *pServer) {
 	pService->addCharacteristic(&m_tempCharacteristic);
 	pService->addCharacteristic(&m_humCharacteristic);
 	pService->addCharacteristic(&m_presCharacteristic);
+  pService->addCharacteristic(&m_iaqCharacteristic);
+  pService->addCharacteristic(&m_siaqCharacteristic);
 	m_tempCharacteristic.addDescriptor(&m_tempDescriptor);
 	m_humCharacteristic.addDescriptor(&m_humDescriptor);
 	m_presCharacteristic.addDescriptor(&m_presDescriptor);
+  m_iaqCharacteristic.addDescriptor(&m_iaqDescriptor);
+  m_siaqCharacteristic.addDescriptor(&m_siaqDescriptor);
 	
 	pService->start();
 	return true;
